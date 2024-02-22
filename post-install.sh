6a36f026a14f59fe36668ea18dd6b1e3138b7c80
lmohesky <158105893+lmohesky@users.noreply.github.com>
2024-02-22 08:27:34 -0600

    Initial commit of post-install.sh

---
+++ post-install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Update and upgrade the system
+sudo apt update
+sudo apt upgrade -y
+
+# Install neofetch
+sudo apt install neofetch -y
+
+# Install btop
+sudo apt install btop -y
+
+# Install kitty
+sudo apt install kitty -y
+
+# Install git
+sudo apt install git -y
